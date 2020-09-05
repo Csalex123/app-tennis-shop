5cc3734d3d8e293e23048e10d5307dfc5e5cbf6f
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-05 20:37:46 -0300

    Menu da home adicionado e estilizado

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
 import Route from './src/route';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Anton_400Regular,
+  });
+
+  
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
     <>
       <StatusBar style="dark" backgroundColor="#000" translucent={true} />
@@ -11,12 +23,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function Home(){
-    return(
-        <View>
-            <Text style={{ marginTop: 60 }}>Página Home</Text>
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={require('../../assets/banner.png')}
+                    style={styles.image}
+                />
+
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>TÊNIS</Text>
+                    <Text style={[styles.text, {color: '#CECECF'}]}>-</Text>
+                    <Text style={[styles.text, {color: '#CECECF'}]}>MASCULINO</Text>
+                    <TouchableOpacity style={{ position: 'absolute', right: 0, alignSelf: 'center' }}>
+                        <MaterialIcons name="filter-list" size={24} color="#000" />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#fff'
+    },
+    header: {
+        marginBottom: 8
+    },
+    image: {
+        width: '100%'
+    },
+
+    textContainer: {
+        flexDirection: 'row',
+        marginVertical: '5%',
+        marginHorizontal: '5%',
+    },
+    text: {
+        fontFamily: 'Anton_400Regular',
+        fontSize: 26,
+        marginHorizontal: '1%'
+    }
+})
